c3b49f4ef52003063ae36062d478c8a943fc14d8
viki3612 <viktorianikolova95@gmail.com>
2022-10-20 17:59:44 +0100

    added new component Phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
     return (
       <div className="Result">
         {/*displaying what is within the api call within 'word'*/}
         <h2 className="result-output"> {props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
