9ec0e50f9d7306c8848a3cb9f82a80956266d50b
viki3612 <viktorianikolova95@gmail.com>
2022-10-21 17:04:52 +0100

    installed font awesome and added audio icon to Phonetics component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        {" "}
-        Listen
+        <FontAwesomeIcon icon={faVolumeHigh} />{" "}
       </a>
-      <br />
       {props.phonetic.text}
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
+
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Result">
-        {/*displaying what is within the api call within 'word'*/}
-        <h2 className="result-output"> {props.results.word}</h2>
+        <h2 className="result-output">
+          {/*displaying what is within the api call within 'word'*/}
+
+          {props.results.word}
+        </h2>
         {props.results.phonetics.map(function (phonetic, index) {
           return (
             <div key={index}>
@@ -14,7 +18,6 @@ export default function Results(props) {
             </div>
           );
         })}
-
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
