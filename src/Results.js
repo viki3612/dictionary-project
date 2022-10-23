fd186d53a9cf907806d5441b5e758fdeb25a0b26
viki3612 <viktorianikolova95@gmail.com>
2022-10-23 21:58:13 +0100

    css changes

--- src/Results.js
+++ src/Results.js
@@ -8,17 +8,19 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Result">
-        <h2 className="result-output">
-          {/*displaying what is within the api call within 'word'*/}
-          {props.results.word}
-        </h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
+        <section>
+          <h2 className="result-output">
+            {/*displaying what is within the api call within 'word'*/}
+            {props.results.word}
+          </h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
 
         <div className="Definitions">
           {props.results.meanings.map(function (meaning, index) {
