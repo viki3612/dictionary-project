43c2c4e96596f2bf6bb2f8bae7684df9ded807fc
viki3612 <viktorianikolova95@gmail.com>
2022-10-25 11:17:12 +0100

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="landscape-photos"
+                  />
                 </a>
               </div>
             );
